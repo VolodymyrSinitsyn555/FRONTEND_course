d0f6446b08dbdaa3ffd4e6fd0794f50a9cd98af4
VolodymyrSinitsyn555 <Vladiofun555@gmail.com>
2024-09-30 17:41:34 +0200

    commit 1

---
+++ HW/hw_09-dom-magic-buttons-main/index.js
@@ -0,0 +1,35 @@
+// // найдем первую кнопку по id
+// const btn = document.getElementById("magic-btn");
+// // скопируем ее - получилась вторая кнопка. Ее можно использовать.
+// const clonedBtn = btn.cloneNode(true);
+// // задали id
+// clonedBtn.id = "magic-btn-2";
+
+// btn.addEventListener('click', () => {
+//   // при нажатии на первую кнопку
+//   // работайте с clonedBtn
+//   // ваш код начинается здесь
+  
+// })
+
+// // здесь можете создать EventListener для второй кнопки
+
+let btn = document.getElementById("magic-btn")
+let clonedBtn = btn.cloneNode(true);
+clonedBtn.className = "magic-btn-2"
+
+btn.addEventListener('click', () => {
+  clonedBtn.style.color = 'wight'
+  clonedBtn.style.backgroundColor = '#a78b71'
+  clonedBtn.innerText = 'Я изменя тебя'
+  document.body.append(clonedBtn)}
+)
+
+clonedBtn.addEventListener('click', () => {
+  btn.style.color = 'black'
+  btn.style.backgroundColor = '#9c4a1a'
+})
+
+
+
+
